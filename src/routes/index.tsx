41350b254ffcbc8ce546e9226278bf541639c1d0
Luis Gabriel Janco <janco7249@gmail.com>
2024-03-01 19:16:33 -0400

    refactor(root): add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,27 @@
 import { Route, Routes as BaseRoutes } from 'react-router-dom'
-import LoginPage from '../pages/login'
+import { PrivateRoutes, PublicRoutes } from '../utils/routes.utils'
 import ProtectedRoute from './protectedRoute'
-import useAuth from '../hooks/useAuth'
-import RegisterPage from '../pages/register'
-import HomePage from '../pages/home'
-import LandingPage from '../pages/landing'
-import { PublicRoutes, PrivateRoutes } from '../utils/constants'
+import Private from './private.routes'
+import { lazy } from 'react'
+
+const LoginPage = lazy(() => import('@/pages/login'))
+const RegisterPage = lazy(() => import('@/pages/register'))
+const LandingPage = lazy(() => import('@/pages/landing'))
 
 const Routes = () => {
-  const { isAuthenticated } = useAuth()
   return (
     <BaseRoutes>
-      {/* RUTAS PÚBLICAS */}
-      <Route index element={<LandingPage />} />
-      {/* RUTAS PÚBLICAS, PERO SI YA ESTÁ AUTENTICADO OCULTAR DICHAS RUTAS */}
-      <Route element={<ProtectedRoute isAllowed={!isAuthenticated} redirectTo={PrivateRoutes.HOME} />}>
+      {/* Rutas públicas, pero si ya está autenticado ocultar dichas rutas */}
+      <Route path={PublicRoutes.LANDING} element={<LandingPage />} />
+      <Route element={<ProtectedRoute redirectTo={PrivateRoutes.DASHBOARD} />}>
         <Route path={PublicRoutes.LOGIN} element={<LoginPage />} />
         <Route path={PublicRoutes.REGISTER} element={<RegisterPage />} />
       </Route>
-      {/* RUTAS PRIVADAS */}
-      <Route element={<ProtectedRoute isAllowed={isAuthenticated} redirectTo={PublicRoutes.LANDING} />}>
-        <Route path={PrivateRoutes.HOME} element={<HomePage />} />
-        <Route path={PrivateRoutes.DASHBOARD} element={<h2>DashboardPage</h2>} />
-        <Route path={PrivateRoutes.TASK} element={<h2>TaskPage</h2>} />
-        <Route path={PrivateRoutes.TASK_ID} element={<h2>TaskPage id</h2>} />
+      {/* Rutas privadas */}
+      <Route element={<ProtectedRoute isPrivate redirectTo={PublicRoutes.LOGIN} />}>
+        <Route path='/*' element={<Private />} />
       </Route>
-      <Route path="*" element={<h2>Not found</h2>} />
-    </BaseRoutes>
+    </BaseRoutes >
   )
 }
 
